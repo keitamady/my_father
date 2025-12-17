45bee6f821258dbab158ae628ac35f3e2418844a
keitamady <sereboria100@gmail.com>
2025-12-17 08:02:25 +0000

    this is for my child mamadi keita

---
+++ femme.js
@@ -0,0 +1,7 @@
+let myName  = "keita moussa kourouma from guinea in conakry";
+myName.onclick = function(){
+    console.log("je suis le premier fils de mon ar")
+}
+function myFirst(){
+    console.log("i am your brother mamadi keita from guinea conakry, and i want to understand why must i be a single man no wife no children and no family")
+}
--- foume.js
+++
@@ -1,4 +0,0 @@
-let myName  = "keita moussa kourouma from guinea in conakry";
-myName.onclick = function(){
-    console.log("je suis le premier fils de mon ar")
-}
